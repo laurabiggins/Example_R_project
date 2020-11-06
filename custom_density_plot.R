d23851113b430084e0d716d5c8b4ccdf99566de5
laurabiggins <laurabiggins10@gmail.com>
2020-11-06 13:55:29 +0000

    Added custom colours.

--- custom_density_plot.R
+++ custom_density_plot.R
@@ -3,7 +3,9 @@ library(ggplot2)
 density_toothGrowth <- function() {
   
   ggplot(ToothGrowth, aes(x = len, fill = supp, color = supp)) +
-    geom_density()
+    geom_density() +
+    scale_color_manual(values = c("#1f9492", "#4322a3")) +
+    scale_fill_manual(values = c("#1f9492", "#4322a3"))
   
 }  
   
