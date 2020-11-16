ff50eb219568d6e9ce8dcef65bbe4c13e44be5fc
yunhui.cao@astrazeneca.com <yunhui.cao@astrazeneca.com>
2020-11-16 12:13:30 -0500

    add title, add axis label

--- custom_boxplot.R
+++ custom_boxplot.R
@@ -4,4 +4,6 @@ iris_boxplot <- function(){
   
   ggplot(iris, aes(x = Species, y = Sepal.Length)) +
     geom_boxplot()
+  +ggtitle("Petal and sepal length of iris")
+  + labs(y="Petal length (cm)", x = "Sepal length (cm)")
 }
