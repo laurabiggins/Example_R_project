3ce270a3f55257d2b87cdb70dd8f7ece82009726
tinyretic <andrzej.prokop@gmail.com>
2020-11-16 18:21:46 +0100

    added jitter plot and some colouring

--- custom_boxplot.R
+++ custom_boxplot.R
@@ -3,5 +3,8 @@ library(ggplot2)
 iris_boxplot <- function(){
   
   ggplot(iris, aes(x = Species, y = Petal.Length)) +
-    geom_boxplot()
+    geom_boxplot(fill="yellow") +
+    geom_jitter(width=0.2)
 }
+
+iris_boxplot()
