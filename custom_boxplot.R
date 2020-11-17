5ca8232ad9a874098e112926de46a7dbadb186d2
nmajoraz <nicole.major@astrazeneca.com>
2020-11-17 14:08:33 -0500

    Added a title to the boxplot.

--- custom_boxplot.R
+++ custom_boxplot.R
@@ -3,7 +3,10 @@ library(ggplot2)
 iris_boxplot <- function(){
   
   ggplot(iris, aes(x = Species, y = Sepal.Length)) +
-    geom_boxplot(size=2, color="blue2")
+    geom_boxplot(size=2, color="blue2") + 
+    ggtitle("Boxplot of Sepal Length by Flower Species")
 }
 
 #Comment from Yawen Yu
+
+iris_boxplot()
